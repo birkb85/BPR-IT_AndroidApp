7a1938318404e42b35342b80ce8fc488832fc87e
Birk Bonnicksen <birkelskerlinette@gmail.com>
2018-10-15 16:02:31 +0200

    Inkluderet basal opsætning af farver og komponenter. Lavet layout til login.

--- app/src/main/java/com/bpr_it/app/bpr_it/ui/login/LoginFragment.kt
+++ app/src/main/java/com/bpr_it/app/bpr_it/ui/login/LoginFragment.kt
@@ -3,18 +3,69 @@ package com.bpr_it.app.bpr_it.ui.login
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bpr_it.app.bpr_it.R
+import android.content.pm.PackageManager
+import android.widget.*
+
 
 class LoginFragment : Fragment() {
+
+    var linearLayout: LinearLayout? = null
+    var usernameEditText: EditText? = null
+    var passwordEditText: EditText? = null
+    var rememberCheckBox: CheckBox? = null
+    var loginButton: Button? = null
+    var versionTextView: TextView? = null
 
     companion object {
         fun newInstance() = LoginFragment()
     }
 
     private lateinit var viewModel: LoginViewModel
+
+    fun showAlert() {
+        // Initialize a new instance of
+        context?.let {
+            val builder = AlertDialog.Builder(it)
+
+            // Set the alert dialog title
+            builder.setTitle("App background color")
+
+            // Display a message on alert dialog
+            builder.setMessage("Are you want to set the app background color to RED?")
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("YES"){dialog, which ->
+                // Do something when user press the positive button
+                Toast.makeText(it,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
+
+                // Change the app background color
+                //root_layout.setBackgroundColor(Color.RED)
+            }
+
+
+            // Display a negative button on alert dialog
+            builder.setNegativeButton("No"){dialog,which ->
+                Toast.makeText(it,"You are not agree.",Toast.LENGTH_SHORT).show()
+            }
+
+
+            // Display a neutral button on alert dialog
+            builder.setNeutralButton("Cancel"){_,_ ->
+                Toast.makeText(it,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
+            }
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -26,6 +77,25 @@ class LoginFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
         // TODO: Use the ViewModel
 
+        // Sæt views
+        linearLayout = activity?.findViewById(R.id.linearLayout)
+        usernameEditText = activity?.findViewById(R.id.usernameEditText)
+        passwordEditText = activity?.findViewById(R.id.passwordEditText)
+        rememberCheckBox = activity?.findViewById(R.id.rememberCheckBox)
+        loginButton = activity?.findViewById(R.id.loginButton)
+        versionTextView = activity?.findViewById(R.id.versionTextView)
+
+        // Vis version
+        try {
+            context?.let {
+                val pInfo = it.packageManager.getPackageInfo(it.packageName, 0)
+                val version = getString(R.string.login_version) + " " + pInfo.versionName
+                versionTextView?.setText(version)
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+//            Global.showExceptionAlertDialog(e, activity, alertDialogs)
+//            Crashlytics.logException(e)
+        }
 
     }
 
